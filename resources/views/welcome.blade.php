05fe4fbbfc97bce182cd83f11c907e28fd31afb7
Rattakarn Noppanutchaiyanan <60143418@g.cmru.ac.th>
2020-07-18 11:59:23 +0700

    edit web1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    paryut
                 </div>
 
                 <div class="links">
